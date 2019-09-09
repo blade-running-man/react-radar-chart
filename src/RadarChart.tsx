7ba2bdd9bee25c4cdbc799da1ddced6e9e9fe725
blade-running-man <oleksandr.slukovskyi@gmail.com>
2019-09-09 22:49:39 +0100

    Create RadarChart.tsx

---
+++ src/RadarChart.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+interface ChartRadarProps {
+}
+
+export default class ChartRadar extends Component<ChartRadarProps, any> {
+
+  render() {
+    return (
+        <svg
+        version="1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+      </svg>
+    );
+  }
+}
