15acba2fe796ce84daac421010f41ee1b2a1e870
blade-running-man <oleksandr.slukovskyi@gmail.com>
2019-09-10 11:56:46 +0300

    Init RadarChart

--- src/RadarChart.tsx
+++ src/RadarChart.tsx
@@ -7,10 +7,7 @@ export default class ChartRadar extends Component<ChartRadarProps, any> {
 
   render() {
     return (
-        <svg
-        version="1"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+      <svg version="1" xmlns="http://www.w3.org/2000/svg">
       </svg>
     );
   }
