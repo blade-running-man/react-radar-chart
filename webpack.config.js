58be34614ab749718b0144f252b2d80905ac36b4
blade-running-man <oleksandr.slukovskyi@gmail.com>
2019-09-17 23:01:14 +0100

    Provide typescript

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var precss  = require('precss');
+var cssnext = require('postcss-cssnext');
+var vars    = require('postcss-simple-vars');
+var nested  = require('postcss-nested');
+var mixins  = require('postcss-mixins');
+
+module.exports = {
+  mode: "production",
+  entry: {
+    app: ['./src/RadarChart.tsx'],
+  },
+  output: {
+    filename: "./index.js",
+    sourceMapFilename: './index.js.map',
+    library: 'RadarChart',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    'react': {
+      'commonjs': 'react',
+      'commonjs2': 'react',
+      'amd': 'react',
+      'root': 'React'
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.js|\.ts|\.tsx?$/,
+      use: ['babel-loader'],
+      exclude: /node_modules/,
+      include: path.join(__dirname, '/src')
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        { loader: 'css-loader' },
+        { loader: 'postcss-loader', options: { plugins: () => [...plugins] } }
+      ]
+    }]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json', '.css'],
+  },
+  resolveLoader: {
+    modules: ['node_modules'],
+  },
+  optimization: {
+    minimize: true
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [precss, cssnext, vars, nested, mixins],
+      },
+    }),
+  ]
+};
